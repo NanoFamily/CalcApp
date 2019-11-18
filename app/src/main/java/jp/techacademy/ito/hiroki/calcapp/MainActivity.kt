6df07ff5639dcd87bbc658067320b9d930c03991
Hitoki <diavolos1230@gmail.com>
2019-11-18 22:43:45 +0900

    2回目コミット。アプリクラッシュ(゜o゜)

--- app/src/main/java/jp/techacademy/ito/hiroki/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ito/hiroki/calcapp/MainActivity.kt
@@ -22,11 +22,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button4.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        if (v.id == R.id.button1) {
-
-        }
+    override fun onClick(v: View) {
         val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+        //★ﾌﾟﾗｽ
+        if (v.id == R.id.button1) {
+            var str1 = editText1.text.toString().toInt()
+
+            var str2 = editText2.text.toString().toInt()
+
+            /*val intent = Intent(this, SecondActivity::class.java)*/
+            intent.putExtra("VALUE1", str1)
+            intent.putExtra("VALUE2", str2)
+            startActivity(intent)
+        }
+        //★マイナス
+        if (v.id == R.id.button2) {
+            var str3 = editText1.text.toString().toInt()
+
+            var str4 = editText2.text.toString().toInt()
+
+            intent.putExtra("VALUE3", str3)
+            intent.putExtra("VALUE4", str4)
+            startActivity(intent)
+        }
+        //★カケル
+        if (v.id == R.id.button3) {
+            var str5 = editText1.text.toString().toInt()
+
+            var str6 = editText2.text.toString().toInt()
+
+            intent.putExtra("VALUE5", str5)
+            intent.putExtra("VALUE6", str6)
+            startActivity(intent)
+        }
+        //★ワル
+        if (v.id == R.id.button4) {
+            var str7 = editText1.text.toString().toInt()
+
+            var str8 = editText2.text.toString().toInt()
+
+            intent.putExtra("VALUE7", str7)
+            intent.putExtra("VALUE8", str8)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/jp/techacademy/ito/hiroki/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/ito/hiroki/calcapp/SecondActivity.kt
@@ -2,6 +2,7 @@ package jp.techacademy.ito.hiroki.calcapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -12,13 +13,22 @@ class SecondActivity : AppCompatActivity() {
         val value1 = intent.getIntExtra("VALUE1", 0)
         val value2 = intent.getIntExtra("VALUE2", 0)
 
-
         textView.text = "${value1 + value2}"
 
-        textView.text = "${value1 - value2}"
+        val value3 = intent.getIntExtra("VALUE3", 0)
+        val value4 = intent.getIntExtra("VALUE4", 0)
 
-        textView.text = "${value1 * value2}"
+        textView.text = "${value3 - value4}"
 
-        textView.text = "${value1 / value2}"
+        val value5 = intent.getIntExtra("VALUE5", 0)
+        val value6 = intent.getIntExtra("VALUE6", 0)
+
+        textView.text = "${value5 * value6}"
+
+        val value7 = intent.getIntExtra("VALUE7", 0)
+        val value8 = intent.getIntExtra("VALUE8", 0)
+
+        textView.text = "${value7 / value8}"
+
     }
 }
